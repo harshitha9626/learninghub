53ae9b2ea687074628d08278c8b60edf2f7b5037
Dhubhasi Harshitha <dhubhasiharshitha09@gmail.com>
2026-03-23 17:49:42 +0530

    Fix password confirmation variable name in validateForm

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function validateForm() {
     let pass = document.getElementById("password").value;
-    let confirm = document.getElementById("confirm").value;
+    let confirmPass = document.getElementById("confirmPassword").value;
     let msg = document.getElementById("msg");
 
-    if (pass !== confirm) {
+    if (pass !== confirmPass) {
         msg.innerHTML = "Passwords do not match!";
         msg.style.color = "red";
         return false;
